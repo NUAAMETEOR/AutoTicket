777c772f7616654e2521fcea6cce99f060f88c4a
NUAAMETEOR <nuaameteor@qq.com>
2018-04-21 00:33:59 +0800

    做了一个js demo解析json

--- app/src/main/java/cn/edu/nuaa/autoticket/model/AsynTicketFetcher.java
+++ app/src/main/java/cn/edu/nuaa/autoticket/model/AsynTicketFetcher.java
@@ -5,9 +5,13 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -79,41 +83,39 @@ public class AsynTicketFetcher extends AsyncTask<QueryParameters, Integer, Array
 
     private String getTrainInformation() {
         HttpURLConnection     urlConnection         = null;
-        ByteArrayOutputStream byteArrayOutputStream = null;
         InputStream           inputStream           = null;
+        BufferedReader        reader                = null;
+        StringBuilder         stringBuilder         = null;
         try {
             //"https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date=2018-04-30&leftTicketDTO.from_station=NJH&leftTicketDTO.to_station=SHH&purpose_codes=ADULT"
             URL url = new URL(makeUrl());
             urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
             urlConnection.setConnectTimeout(10000);
-            byteArrayOutputStream = new ByteArrayOutputStream();
             urlConnection.connect();
             if (urlConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {
                 return null;
             }
             inputStream = urlConnection.getInputStream();
-            int    readCount = 0;
-            byte[] content   = new byte[1024];
-            while ((readCount = inputStream.read(content, 0, 1024)) > 0) {
-                byteArrayOutputStream.write(content, 0, readCount);
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            stringBuilder = new StringBuilder();
+            String content;
+            while ((content = reader.readLine()) != null) {
+                stringBuilder.append(content);
+                stringBuilder.append('\n');
             }
-            return new String(byteArrayOutputStream.toByteArray());
+            content = stringBuilder.toString();
+            content = content.substring(0, content.length() - 1);
+            return content;
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             if (urlConnection != null) {
                 urlConnection.disconnect();
             }
-            if (inputStream != null) {
+            if (reader != null) {
                 try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (byteArrayOutputStream != null) {
-                try {
-                    byteArrayOutputStream.close();
+                    reader.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -127,7 +129,7 @@ public class AsynTicketFetcher extends AsyncTask<QueryParameters, Integer, Array
         InputStream inputStream = null;
 //        ResourceBundle resourceBundle = null;
         try {
-            inputStream = context.getAssets().open("city_code_zh.properties");
+            inputStream = context.getAssets().open("city_code.properties");
 //            resourceBundle = new PropertyResourceBundle(inputStream);
             properties.load(inputStream);
             inputStream.close();
@@ -135,5 +137,68 @@ public class AsynTicketFetcher extends AsyncTask<QueryParameters, Integer, Array
             e.printStackTrace();
         }
         return properties;
+    }
+
+    private void parseTrainInformation(String trainInfo) {
+        Gson gson=new Gson();
+        gson.fromJson(trainInfo, TicketHolder.class);
+        /*var trainObject = JSON.parse(trainStr);
+        if (!trainObject.status) {
+            console.log("train information invalid");
+            return;
+        }
+        var data = trainObject.data;
+        var result = data.result;
+        var flag = data.flag;
+        var map = data.map;
+        var trainnum = result.length;
+        var table = document.getElementById("tb");
+        console.log("receive " + trainnum + " train information");
+        for (var i = 1; i <= trainnum; i++) {
+            var tickect = {};
+            var temp = result[i].split("|");
+
+            tickect.trainId = temp[3];// Train ID
+            tickect.startTime = temp[8];// Start Time
+            tickect.endTime = temp[9];// End Time
+            tickect.totalTime = temp[10];// Total Time
+            tickect.date = temp[13];
+            tickect.ruanwo = temp[23];// 软卧
+            tickect.ruanzuo = temp[24]; // 软座
+            tickect.wuzuo = temp[26];// 无座
+            tickect.yingwo = temp[28]; // 硬卧
+            tickect.yingzuo = temp[29]; // 硬座
+            tickect.scSeat = temp[30];// 二等座
+            tickect.fcSeat = temp[31];// 一等座
+            tickect.bcSeat = temp[32];// 商务座 / 特等座
+            tickect.dongwo = temp[33];// 动卧
+
+            for(var prop in tickect){
+                if(tickect[prop].length==0) {
+                    tickect[prop] = "--";
+                }
+            }
+
+            console.log(tickect);
+            var row=table.insertRow(i);
+            for(var j=0;j<14;j++) {
+                row.insertCell(j);
+            }
+            row.cells[0].innerHTML=tickect.trainId;
+            row.cells[1].innerHTML=tickect.startTime;
+            row.cells[2].innerHTML=tickect.endTime;
+            row.cells[3].innerHTML=tickect.totalTime;
+            row.cells[4].innerHTML=tickect.date;
+            row.cells[5].innerHTML=tickect.ruanwo;
+            row.cells[6].innerHTML=tickect.ruanzuo;
+            row.cells[7].innerHTML=tickect.wuzuo;
+            row.cells[8].innerHTML=tickect.yingwo;
+            row.cells[9].innerHTML=tickect.yingzuo;
+            row.cells[10].innerHTML=tickect.scSeat;
+            row.cells[11].innerHTML=tickect.fcSeat;
+            row.cells[12].innerHTML=tickect.bcSeat;
+            row.cells[13].innerHTML=tickect.dongwo;
+        }
+    }*/
     }
 }
---
+++ app/src/main/java/cn/edu/nuaa/autoticket/model/TicketHolder.java
@@ -0,0 +1,11 @@
+package cn.edu.nuaa.autoticket.model;
+
+/**
+ * @author Meteor
+ * @version V1.0
+ * @ClassName: TicketHolder.java
+ * @Description:
+ * @Date 2018/4/21 0:24
+ */
+public class TicketHolder {
+}
